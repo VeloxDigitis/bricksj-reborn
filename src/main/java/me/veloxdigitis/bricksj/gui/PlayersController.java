d7722982d45fb858f55ee87a1196864a7a7c1e8b
Krzysztof Cybulski <krzysztofpcy@gmail.com>
2017-12-26 18:08:01 +0100

    Load players from directories

--- src/main/java/me/veloxdigitis/bricksj/gui/PlayersController.java
+++ src/main/java/me/veloxdigitis/bricksj/gui/PlayersController.java
@@ -1,13 +1,13 @@
 package me.veloxdigitis.bricksj.gui;
 
 import javafx.beans.binding.Bindings;
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Window;
 import me.veloxdigitis.bricksj.battle.BrickPlayer;
@@ -17,8 +17,13 @@ import me.veloxdigitis.bricksj.proxy.BricksAlgorithm;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class PlayersController {
 
@@ -34,19 +39,28 @@ public class PlayersController {
     @FXML
     public void initialize() {
         this.playersAmount.textProperty().bind(Bindings.size(players).asString().concat(" players"));
-        playersList.setItems(FXCollections.observableArrayList(players));
+        playersList.setItems(players);
     }
 
     @FXML
     public void addPlayer(ActionEvent event) {
         loadAlgorithm(((Node)event.getTarget()).getScene().getWindow()).ifPresent(players::add);
-        playersList.setItems(FXCollections.observableArrayList(players));
+    }
+
+    @FXML
+    public void addDir(ActionEvent event) {
+        players.addAll(loadAlgorithms(new DirectoryChooser().showDialog(((Node) event.getTarget()).getScene().getWindow()).toPath()));
+    }
+
+    @FXML
+    private void loadDesktop() {
+        players.addAll(loadAlgorithms(Paths.get(System.getProperty("user.home"), "Desktop")));
     }
 
     private Optional<BricksAlgorithm> loadAlgorithm(Window window) {
         FileChooser chooser = new FileChooser();
-        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Algorithm filem (*.info)", "*.info");
-        chooser.getExtensionFilters().add(extFilter);
+        chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Algorithm files (*.info)", "*.info"));
+        chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Algorithm files (*.txt)", "*.txt"));
         chooser.setTitle("Choose algorithm");
 
         File file = chooser.showOpenDialog(window);
@@ -57,6 +71,30 @@ public class PlayersController {
         }
 
         return Optional.empty();
+    }
+
+    private List<BricksAlgorithm> loadAlgorithms(Path directory) {
+        try {
+            return Files.walk(directory).
+                    filter(p -> p.toString().endsWith(".info")
+                            || p.toString().endsWith(".txt")).
+                    map(this::algorithmFromPath).
+                    filter(Optional::isPresent).
+                    map(Optional::get).
+                    collect(Collectors.toList());
+        } catch (IOException e) {
+            Logger.error("Error while walking directory");
+            return Collections.emptyList();
+        }
+    }
+
+    private Optional<BricksAlgorithm> algorithmFromPath(Path path) {
+        try {
+            return Optional.of(new BricksAlgorithm(new InfoFileLoader().load(path)));
+        } catch (IOException e) {
+            Logger.error("Couldn't load " + path);
+            return Optional.empty();
+        }
     }
 
     @FXML
