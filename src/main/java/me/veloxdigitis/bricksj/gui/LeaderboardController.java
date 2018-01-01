11787a7980cea49258d55382f48cf907a965cbe5
damian <damianr1602@gmail.com>
2018-01-01 17:14:07 +0100

    small changes in stats

--- src/main/java/me/veloxdigitis/bricksj/gui/LeaderboardController.java
+++ src/main/java/me/veloxdigitis/bricksj/gui/LeaderboardController.java
@@ -4,6 +4,7 @@ import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
@@ -12,9 +13,9 @@ import javafx.scene.control.TableView;
 import javafx.util.StringConverter;
 import me.veloxdigitis.bricksj.leaderboard.Leaderboard;
 import me.veloxdigitis.bricksj.leaderboard.Stats;
-import org.junit.FixMethodOrder;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -22,13 +23,17 @@ public class LeaderboardController {
 
     private final Leaderboard leaderboard;
 
-    @FXML private TableView<Stats> playersTable;
+    @FXML
+    private TableView<Stats> playersTable;
 
-    @FXML private CategoryAxis alghName;
+    @FXML
+    private CategoryAxis alghName;
 
-    @FXML private NumberAxis wins;
+    @FXML
+    private NumberAxis wins;
 
-    @FXML private BarChart<String, Number> bc;
+    @FXML
+    private BarChart<String, Number> bc;
 
 
     public LeaderboardController(Leaderboard leaderboard) {
@@ -37,31 +42,44 @@ public class LeaderboardController {
 
     @FXML
     private void initialize() {
-        wins.setTickUnit(10);
+        this.playersTable.setItems(FXCollections.observableList(leaderboard.getPlayersWithStats()));
+        setChartBar();
+    }
+
+
+    private void setChartBar() {
         wins.setTickLabelFormatter(new StringConverter<>() {
                                        @Override
                                        public String toString(Number object) {
                                            return String.valueOf(object.intValue() == object.doubleValue() ? object.intValue() : "");
                                        }
+
                                        @Override
                                        public Number fromString(String string) {
                                            return Integer.parseInt(string);
                                        }
                                    }
         );
-
-        this.playersTable.setItems(FXCollections.observableList(leaderboard.getPlayersWithStats()));
         XYChart.Series<String, Number> series = new XYChart.Series<>();
         ObservableList<XYChart.Series<String, Number>> answer = FXCollections.observableArrayList();
         List<Number> result = leaderboard.getPlayersWithStats().stream().map(Stats::getWins).collect(Collectors.toList());
         List<String> names = leaderboard.getPlayersWithStats().stream().map(Stats::getPlayerName).collect(Collectors.toList());
 
-        for (int j = 0; j < names.size() && !names.isEmpty(); j++) {
+
+        for (int j = 0; j < names.size(); j++) {
             final int counter = j;
-            Platform.runLater(() -> series.getData().add(new XYChart.Data<>(names.get(counter), result.get(counter))) );
+            Platform.runLater(() -> {
+                final XYChart.Data<String, Number> data = new XYChart.Data<>(names.get(counter), result.get(counter));
+                series.getData().add(data);
+
+            });
         }
+
         answer.add(series);
         bc.setData(answer);
+
+
     }
+
 
 }
